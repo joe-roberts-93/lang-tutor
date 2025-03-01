0bc1846ea3cc2e698c47880e6280b4b24cd4195b
Joe Roberts <roberts.josephpaul@gmail.com>
2025-03-01 14:51:31 -0500

    Made automatic rubocop corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate_request
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
+    header = request.headers["Authorization"]
+    header = header.split(" ").last if header
     decoded = jwt_decode(header)
     @current_user = User.find(decoded[:user_id])
   rescue ActiveRecord::RecordNotFound => e
--- app/models/concerns/json_web_token.rb
+++ app/models/concerns/json_web_token.rb
@@ -1,14 +1,13 @@
-require 'jwt'
+require "jwt"
 
 module JsonWebToken
-
   def jwt_encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, ENV['JWT_SECRET_KEY'])
+    JWT.encode(payload, ENV["JWT_SECRET_KEY"])
   end
 
   def jwt_decode(token)
-    decoded = JWT.decode(token, ENV['JWT_SECRET_KEY'])[0]
+    decoded = JWT.decode(token, ENV["JWT_SECRET_KEY"])[0]
     HashWithIndifferentAccess.new decoded
   end
 end
